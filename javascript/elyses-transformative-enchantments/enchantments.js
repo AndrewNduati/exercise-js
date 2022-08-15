1ff3ea92b28f14cedaab57f8c75ffea428373907
AndrewNduati <andrew.njage@meltwater.org>
2022-08-16 01:14:12 +0300

    initial commit

---
+++ javascript/elyses-transformative-enchantments/enchantments.js
@@ -0,0 +1,93 @@
+// @ts-check
+
+/**
+ * Double every card in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with every card doubled
+ */
+export function seeingDouble(deck) {
+  return deck.map((item) => item*2)
+}
+
+/**
+ *  Creates triplicates of every 3 found in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with triplicate 3s
+ */
+export function threeOfEachThree(deck) {
+  // Find the 3 then add two more threes in the indices after
+  let idx = deck.indexOf(3)
+  while (idx != -1) {
+    deck.splice(idx, 0, 3,3)
+    idx = deck.indexOf(3, idx+3)
+  }
+  return deck
+}
+
+/**
+ * Extracts the middle two cards from a deck.
+ * Assumes a deck is always 10 cards.
+ *
+ * @param {number[]} deck of 10 cards
+ *
+ * @returns {number[]} deck with only two middle cards
+ */
+export function middleTwo(deck) {
+  return [deck[Math.floor((deck.length/2)-1)], deck[Math.floor(deck.length/2)]]
+}
+
+/**
+ * Moves the outside two cards to the middle.
+ *
+ * @param {number[]} deck with even number of cards
+ *
+ * @returns {number[]} transformed deck
+ */
+
+export function sandwichTrick(deck) {
+  deck.splice(Math.floor((deck.length/2)), 0, deck[deck.length-1], deck[0])
+  // remove last item 
+  deck.pop()
+  // remove first item
+  deck.shift()
+  return deck
+}
+
+/**
+ * Removes every card from the deck except 2s.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with only 2s
+ */
+export function twoIsSpecial(deck) {
+  return deck.filter(item => item === 2)
+}
+
+/**
+ * Returns a perfectly order deck from lowest to highest.
+ *
+ * @param {number[]} deck shuffled deck
+ *
+ * @returns {number[]} ordered deck
+ */
+export function perfectlyOrdered(deck) {
+  // Sort function when dealing with integers requires a compare function.
+  // Reason I think is because sort compares values as strings
+  return deck.sort(function(a, b){return a-b})
+}
+
+/**
+ * Reorders the deck so that the top card ends up at the bottom.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} reordered deck
+ */
+export function reorder(deck) {
+  return deck.reverse()
+}
