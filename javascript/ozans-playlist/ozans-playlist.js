1c2936f54194a2dd0156312a36b983508e3db177
AndrewNduati <andrew.njage@meltwater.org>
2022-08-24 07:29:15 +0300

    Unlocked sets

---
+++ javascript/ozans-playlist/ozans-playlist.js
@@ -0,0 +1,64 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Removes duplicate tracks from a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} new playlist with unique entries
+ */
+export function removeDuplicates(playlist) {
+  const unique = new Set(playlist)
+  return [...unique];
+}
+
+/**
+ * Checks whether a playlist includes a track.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {boolean} whether the track is in the playlist
+ */
+export function hasTrack(playlist, track) {
+  return playlist.includes(track)  
+}
+
+/**
+ * Adds a track to a playlist.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {string[]} new playlist
+ */
+export function addTrack(playlist, track) {
+  const final_playlist = Array.from(new Set(playlist).add(track))
+  return final_playlist
+}
+
+/**
+ * Deletes a track from a playlist.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {string[]} new playlist
+ */
+export function deleteTrack(playlist, track) {
+  const set_playlist = new Set(playlist)
+  set_playlist.delete(track)
+  return [...set_playlist]
+}
+
+/**
+ * Lists the unique artists in a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} list of artists
+ */
+export function listArtists(playlist) {
+  const artists = new Set()
+  playlist.forEach( item => artists.add(item.split('- ')[1]))
+  return [...artists]
+}
